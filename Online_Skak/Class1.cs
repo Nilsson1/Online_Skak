e6c6523916ead71a3c8f2f128c5b567e2c1000b9
frede <frede@DESKTOP-2E797VH>
2019-03-06 15:54:00 +0100

    dynamically created buttons

---
+++ Online_Skak/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Skak
+{
+    class Class1 : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ Online_Skak/DynamicGridBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Online_Skak
+{
+    public class DynamicGridBuilder
+    {
+        MainWindow Form = Application.Current.Windows[0] as MainWindow;
+
+        public void TextBlock()
+        { 
+            for(int i = 0; i<8; i++) 
+            { 
+                for(int j = 1; j< 9; j++) 
+                { 
+                    Button txt = new Button();
+                    txt.Content = "Cell: " + i + "," + j;
+                    Grid.SetRow(txt, i);
+                    Grid.SetColumn(txt, j);
+                    Form.GridName.Children.Add(txt);
+                }
+            }
+        }
+    }
+}
+

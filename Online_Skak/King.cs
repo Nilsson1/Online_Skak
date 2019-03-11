f4014adfb6572c0241f6f63919d1b86c457eb11f
Mikkel Berg Eriksen <eriksen127@msn.com>
2019-03-11 20:48:42 +0100

    King.cs added

---
+++ Online_Skak/King.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Online_Skak
+{
+    public class King : ButtonClass
+    {
+
+        MainWindow Form = Application.Current.Windows[0] as MainWindow;
+        public King(int row, int column)
+        {
+            Button kingButton = new Button();
+
+            SetButtonPosition(kingButton, row, column);
+
+
+            kingButton.PreviewMouseLeftButtonDown += Btn_PreviewMouseLeftButtonDown;
+            kingButton.PreviewMouseLeftButtonUp += Btn_PreviewMouseLeftButtonUp;
+
+            SetDefaultButtonColor(kingButton, row, column);
+
+            SetButtonName(kingButton, "King");
+            kingButton.Content = "King";
+
+            Form.GridName.Children.Add(kingButton);
+        }
+
+        private void Btn_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (CheckIfBoard(sender)) return;
+
+            row = GetRow(e);
+            column = GetColumn(e);
+
+            if (!(ValidMove(row, column, InitRow, InitCol))) return;
+
+            SwapTwoButtons(e);
+        }
+    }
+}

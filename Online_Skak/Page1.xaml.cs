a502c6187ad3078df934291025f795c46713569b
Alparslan Barbaros Ari <s175186@win.dtu.dk>
2019-05-07 14:06:31 +0200

    Implementeret en timer på en label, mangler at blive implementeret i spillet

--- Online_Skak/Page1.xaml.cs
+++ Online_Skak/Page1.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Online_Skak
 {
@@ -20,6 +21,7 @@ namespace Online_Skak
     /// </summary>
     public partial class Page1 : Page
     {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
         public Page1()
         {
             InitializeComponent();
@@ -40,6 +42,44 @@ namespace Online_Skak
         private void ExitGame_Click(object sender, RoutedEventArgs e)
         {
            Application.Current.MainWindow.Close();
+        }
+
+      /*  private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
+            // Updating the Label which displays the current second
+            test.Content = DateTime.Now.Second;
+
+            // Forcing the CommandManager to raise the RequerySuggested event
+            CommandManager.InvalidateRequerySuggested();
+        }*/
+
+        private void Time_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1.0);
+            dispatcherTimer.Tick += dtTicker;
+            dispatcherTimer.Start();
+
+            
+        }
+
+        private int increment = 11;
+        private void dtTicker(object sender, EventArgs e)
+        {
+            increment--;
+            time.Content = increment.ToString();
+            if ((String)time.Content == "5")
+            {
+                dispatcherTimer.Stop();
+            }
+            dispatcherTimer.Start();               
         }
     }
 }
